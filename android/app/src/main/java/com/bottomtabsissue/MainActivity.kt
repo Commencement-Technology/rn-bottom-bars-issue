c97fcd271985d2a2d032d5f8e1f3289056dbc613
Mathieu Acthernoene <zoontek@gmail.com>
2024-10-30 18:50:18 +0100

    Add reproduction

--- android/app/src/main/java/com/bottomtabsissue/MainActivity.kt
+++ android/app/src/main/java/com/bottomtabsissue/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.bottomtabsissue
 
+import android.graphics.Color
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +22,15 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    // This breaks react-native-bottom-tabs 👇
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+
+    // These lines are not responsible, it's just to see better
+    window.statusBarColor = Color.TRANSPARENT
+    window.navigationBarColor = Color.TRANSPARENT
+  }
 }
